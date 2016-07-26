6a0696630160a51594d397a6a8cf2cac368565b1
Donald May <donaldmay@acm.org>
2016-07-26 14:10:25 -0400

    Updated controller since waypoints has been removed.

--- SailboatAI/controllers.py
+++ SailboatAI/controllers.py
@@ -7,7 +7,6 @@ class SailboatAIController(object):
         self.delta_time = 0
         self.environment = Environment()
         self.is_simulation = False
-        self.waypoints = []
 
     def update(self, status_data_json):
         boat_json = status_data_json['boat']
@@ -15,7 +14,6 @@ class SailboatAIController(object):
         self.delta_time = status_data_json['dt']
         self.environment.load(status_data_json['environment'])
         self.is_simulation = status_data_json['isSimulation']
-        self.waypoints = status_data_json['waypoints']
 
     def determine_control_output(self):
         pass
